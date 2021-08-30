a5b5f4c5881ed423d8f1083c54b230db2e4950e8
Ben Hermann <ben.hermann@googlemail.com>
2021-08-31 00:13:34 +0200

    Added RIndex page

--- department-of-reuse/src/components/TopFive.vue
+++ department-of-reuse/src/components/TopFive.vue
@@ -1,132 +1,214 @@
 <template>
   <div>
-    <h2 class="text-base bg-opacity-80 bg-blue-200">Researchers<button class="float-right" v-on:click="openResearchers = !openResearchers">{{ openResearchers ? "🔼" : "🔽"  }}</button></h2>
-    
+    <h2 class="text-base bg-opacity-80 bg-blue-200">
+      Researchers<button
+        class="float-right"
+        v-on:click="openResearchers = !openResearchers"
+      >
+        {{ openResearchers ? "🔼" : "🔽" }}
+      </button>
+    </h2>
+
     <div v-if="openResearchers">
-    <h3 class="text-sm bg-opacity-40 bg-blue-200">Most reused (R+)</h3>
+      <h3 class="text-sm bg-opacity-40 bg-blue-200">
+        <router-link
+          class="hover:bg-blue-50"
+          :to="{
+            name: 'rindex',
+            params: {
+              target: 'researcher',
+              mode: 'reused',
+            },
+          }"
+          >Most reused (R+)</router-link
+        >
+      </h3>
       <div v-if="isLoading">
         <p>Loading...</p>
       </div>
       <div v-else>
         <table class="m-1 table-auto text-xs w-full bg-opacity-40 bg-white">
-          <tr v-for="(p, index) in researchers.reused" :key="index" :class="index % 2 ? 'bg-opacity-80 bg-white' : ''">
-            <td class="text-left hover:bg-blue-50">{{p.entry.given}} {{p.entry.family}}</td>
-            <td>{{p.frequency}}</td>
+          <tr
+            v-for="(p, index) in researchers.reused"
+            :key="index"
+            :class="index % 2 ? 'bg-opacity-80 bg-white' : ''"
+          >
+            <td class="text-left hover:bg-blue-50">
+              {{ p.entry.given }} {{ p.entry.family }}
+            </td>
+            <td>{{ p.frequency }}</td>
           </tr>
         </table>
       </div>
-    <h3 class="text-sm bg-opacity-40 bg-blue-200">Most reusing (R)</h3>
+      <h3 class="text-sm bg-opacity-40 bg-blue-200">
+        <router-link
+          class="hover:bg-blue-50"
+          :to="{
+            name: 'rindex',
+            params: {
+              target: 'researcher',
+              mode: 'reusing',
+            },
+          }"
+          >Most reusing (R)</router-link
+        >
+      </h3>
       <div v-if="isLoading">
         <p>Loading...</p>
       </div>
-      <div v-else>    
+      <div v-else>
         <table class="m-1 table-auto text-xs w-full bg-opacity-40 bg-white">
-          <tr v-for="(p, index) in researchers.reusing" :key="index" :class="index % 2 ? 'bg-opacity-80 bg-white' : ''">
-            <td class="text-left hover:bg-blue-50">{{p.entry.given}} {{p.entry.family}}</td>
-            <td>{{p.frequency}}</td>
+          <tr
+            v-for="(p, index) in researchers.reusing"
+            :key="index"
+            :class="index % 2 ? 'bg-opacity-80 bg-white' : ''"
+          >
+            <td class="text-left hover:bg-blue-50">
+              {{ p.entry.given }} {{ p.entry.family }}
+            </td>
+            <td>{{ p.frequency }}</td>
           </tr>
         </table>
       </div>
     </div>
-    <h2 class="text-base bg-opacity-80 bg-blue-200 mt-3">Artifacts<button class="float-right" v-on:click="openArtifacts = !openArtifacts">{{ openArtifacts ? "🔼" : "🔽"  }}</button></h2>
+    <h2 class="text-base bg-opacity-80 bg-blue-200 mt-3">
+      Artifacts<button
+        class="float-right"
+        v-on:click="openArtifacts = !openArtifacts"
+      >
+        {{ openArtifacts ? "🔼" : "🔽" }}
+      </button>
+    </h2>
 
     <div v-if="openArtifacts">
-    <h3 class="text-sm bg-opacity-40 bg-blue-200">Most reused (R+)</h3>
+      <h3 class="text-sm bg-opacity-40 bg-blue-200"><router-link
+          class="hover:bg-blue-50"
+          :to="{
+            name: 'rindex',
+            params: {
+              target: 'artifact',
+              mode: 'reused',
+            },
+          }"
+          >Most reused (R+)</router-link></h3>
       <div v-if="isLoading">
         <p>Loading...</p>
       </div>
-      <div v-else>    
+      <div v-else>
         <table class="m-1 table-auto text-xs bg-opacity-40 bg-white">
-          <tr v-for="(p, index) in publications.reused" :key="index" :class="index % 2 ? 'bg-opacity-80 bg-white' : ''">
+          <tr
+            v-for="(p, index) in publications.reused"
+            :key="index"
+            :class="index % 2 ? 'bg-opacity-80 bg-white' : ''"
+          >
             <td class="text-left">
               <router-link
-              class="hover:bg-blue-50"
-              :to="{
+                class="hover:bg-blue-50"
+                :to="{
                   name: 'paper',
                   params: {
-                  doiPrefix: p.entry.dOI.split('/')[0],
-                  doiSuffix: p.entry.dOI.split('/')[1],
+                    doiPrefix: p.entry.dOI.split('/')[0],
+                    doiSuffix: p.entry.dOI.split('/')[1],
                   },
-              }"
+                }"
               >
-              {{ p.entry.title[0] }}
+                {{ p.entry.title[0] }}
               </router-link>
             </td>
-            <td>{{p.frequency}}</td>
+            <td>{{ p.frequency }}</td>
           </tr>
         </table>
       </div>
-    <h3 class="text-sm bg-opacity-40 bg-blue-200">Most reusing (R)</h3>
+      <h3 class="text-sm bg-opacity-40 bg-blue-200"><router-link
+          class="hover:bg-blue-50"
+          :to="{
+            name: 'rindex',
+            params: {
+              target: 'artifact',
+              mode: 'reusing',
+            },
+          }"
+          >Most reusing (R)</router-link></h3>
       <div v-if="isLoading">
         <p>Loading...</p>
       </div>
-      <div v-else>    
+      <div v-else>
         <table class="m-1 table-auto text-xs bg-opacity-40 bg-white">
-          <tr v-for="(p, index) in publications.reusing" :key="index" :class="index % 2 ? 'bg-opacity-80 bg-white' : ''">
+          <tr
+            v-for="(p, index) in publications.reusing"
+            :key="index"
+            :class="index % 2 ? 'bg-opacity-80 bg-white' : ''"
+          >
             <td class="text-left">
               <router-link
-              class="hover:bg-blue-50"
-              :to="{
+                class="hover:bg-blue-50"
+                :to="{
                   name: 'paper',
                   params: {
-                  doiPrefix: p.entry.dOI.split('/')[0],
-                  doiSuffix: p.entry.dOI.split('/')[1],
+                    doiPrefix: p.entry.dOI.split('/')[0],
+                    doiSuffix: p.entry.dOI.split('/')[1],
                   },
-              }"
+                }"
               >
-              {{ p.entry.title[0] }}
+                {{ p.entry.title[0] }}
               </router-link>
             </td>
-            <td>{{p.frequency}}</td>
+            <td>{{ p.frequency }}</td>
           </tr>
-          </table>
+        </table>
       </div>
-      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { ref, onBeforeMount, PropType } from "vue";
 
-import Reuse from '../backend/models/Reuse';
+import Reuse from "../backend/models/Reuse";
 
-import RIndex from '../backend/RIndex';
-import { HistogramEntry } from '../tools/Histogram';
-import { Author, Work } from '../clients/crossref';
+import RIndex from "../backend/RIndex";
+import { HistogramEntry } from "../tools/Histogram";
+import { Author, Work } from "../clients/crossref";
 
 interface HistogramContainer<T> {
-  reused : Array<HistogramEntry<T>>,
-  reusing : Array<HistogramEntry<T>>
+  reused: Array<HistogramEntry<T>>;
+  reusing: Array<HistogramEntry<T>>;
 }
 
 export default {
   props: {
     reuseData: Array as PropType<Array<Reuse>>,
   },
-  setup(props : any) {
+  setup(props: any) {
     const isLoading = ref(false);
     const researchers = ref({} as HistogramContainer<Author>);
     const publications = ref({} as HistogramContainer<Work>);
     const openResearchers = ref(true);
     const openArtifacts = ref(true);
-    
+
     onBeforeMount(async () => {
       isLoading.value = true;
       const indexer = new RIndex(props.reuseData);
-      
-      researchers.value = { 
-        reused: (await indexer.computeAuthorsReused()).slice(0,5),
-        reusing: (await indexer.computeAuthorsReusing()).slice(0,5)
-        };
 
-      publications.value = { 
-        reused: (await indexer.computeWorksReused()).slice(0,5),
-        reusing: (await indexer.computeWorksReusing()).slice(0,5)
-        };
+      researchers.value = {
+        reused: (await indexer.computeAuthorsReused()).slice(0, 5),
+        reusing: (await indexer.computeAuthorsReusing()).slice(0, 5),
+      };
+
+      publications.value = {
+        reused: (await indexer.computeWorksReused()).slice(0, 5),
+        reusing: (await indexer.computeWorksReusing()).slice(0, 5),
+      };
 
       isLoading.value = false;
     });
 
-    return { isLoading, researchers, publications, openResearchers, openArtifacts };
-  }
-}
+    return {
+      isLoading,
+      researchers,
+      publications,
+      openResearchers,
+      openArtifacts,
+    };
+  },
+};
 </script>
---
+++ department-of-reuse/src/pages/RIndex.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <h1 class="text-xl bg-opacity-80 bg-blue-200">
+        <span v-if="target == 'researcher'">Researchers</span>
+        <span v-if="target == 'artifact'">Artifacts</span> 
+         - 
+        <span v-if="mode == 'reusing'">Most reusing (R)</span>
+        <span v-if="mode == 'reused'">Most reused (R+)</span>
+    </h1>
+    <div v-if="isLoading">
+      <p>Loading data ...</p>
+    </div>
+    <div v-else class="pb-10">
+      <DataTable :rows="rows" :columns="columns" class="mx-auto" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from "@vue/reactivity";
+import { onBeforeMount, watch } from "@vue/runtime-core";
+import RIndex from "@/backend/RIndex";
+import reuseJson from "../assets/data/reuse.json";
+import Reuse, { ReuseFromJson } from "@/backend/models/Reuse";
+import { HistogramEntry } from "../tools/Histogram";
+import { Author, Work } from "../clients/crossref";
+import DataTable from "@/components/DataTable.vue";
+import { useRoute } from "vue-router";
+
+export default {
+  name: "RIndex Page",
+  components: { DataTable },
+  setup() {
+    const isLoading = ref(false);
+    const route = useRoute();
+
+    const target = ref(route.params.target as string);
+    const mode = ref(route.params.mode as string);
+
+    const columns = [
+      {
+        prop: "name",
+        name: "Name",
+        class: "text-left align-top overflow-hidden break-all",
+      },
+      {
+        prop: "frequency",
+        name: "Frequency",
+        class: "text-left align-top",
+      },
+    ];
+
+    const rows = ref(new Array<any>());
+    const reuseData = (reuseJson as Array<any>).map(ReuseFromJson);
+
+    onBeforeMount(async () => {
+      await generateRows(reuseData);
+    });
+
+    watch(
+      () => route.params,
+      async (newParams) => {
+        newParams;
+        target.value = route.params.target as string;
+        mode.value = route.params.mode as string;
+        await generateRows(reuseData);
+      }
+    );
+
+    async function generateRows(reuseData: Array<Reuse>) {
+      isLoading.value = true;
+      const indexer = new RIndex(reuseData);
+
+      const target = route.params.target as string;
+      const mode = route.params.mode as string;
+
+      if (target == "researcher") {
+        var researchers: Array<HistogramEntry<Author>> = new Array();
+
+        if (mode == "reused") {
+          researchers = await indexer.computeAuthorsReused();
+        } else if (mode == "reusing") {
+          researchers = await indexer.computeAuthorsReusing();
+        }
+
+        rows.value = researchers.map((e) => {
+          return {
+            name: `${e.entry.given} ${e.entry.family}`,
+            frequency: e.frequency,
+          };
+        });
+      } else if (target == "artifact") {
+        var publications: Array<HistogramEntry<Work>> = new Array();
+
+        if (mode == "reused") {
+          publications = await indexer.computeWorksReused();
+        } else if (mode == "reusing") {
+          publications = await indexer.computeWorksReusing();
+        }
+
+        rows.value = publications.map((e) => {
+          return {
+            name: `${e.entry.title.join(":")} ${e.entry.subtitle ? e.entry.subtitle!.join(":") : ""} `,
+            frequency: e.frequency,
+          };
+        });
+      }
+      isLoading.value = false;
+    }
+
+    return { isLoading, columns, rows, mode, target };
+  },
+};
+</script>
+
+
