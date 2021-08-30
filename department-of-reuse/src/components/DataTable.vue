21bf15260f8f6197b2c82896106cf250262e80af
Ben Hermann <ben.hermann@googlemail.com>
2021-08-30 13:59:10 +0200

    Implemented filter (not beautiful yet)

--- department-of-reuse/src/components/DataTable.vue
+++ department-of-reuse/src/components/DataTable.vue
@@ -2,11 +2,13 @@
   <table class="table-auto text-sm m-2">
     <tr>
       <th
-        v-for="column in columns"
+        v-for="column in internalColumns"
         v-bind:key="column"
         class="border bg-blue-500 text-gray-50"
         :class="column.class"
       >
+        <button class="float-left">🔎</button>
+        <input type="search" v-model="column.filter" class="text-black w-3/4 p-1">
         <button
           v-on:click="if (sortCriterion == column.prop) {sortAscending = !sortAscending; } else { sortAscending = true } sortCriterion = column.prop;"
           class="text-gray-50 font-bold"
@@ -35,6 +37,7 @@ export interface ColumnDefinition {
   prop: String;
   name: String;
   class: String;
+  filter: String;
 }
 
 export default {
@@ -46,26 +49,40 @@ export default {
     const sortCriterion = ref("");
     const sortAscending = ref(true);
     const internalRows = ref([] as Array<any>);
+    const internalColumns = ref([] as Array<ColumnDefinition>);
 
     onBeforeMount(async () => {
       internalRows.value = props.rows;
+      internalColumns.value = props.columns;
+      internalColumns.value.map(e => {e.filter = ""; return e});
     });
 
-    watch([sortCriterion, sortAscending], (newValues, prevValues) => {
+    watch([sortCriterion, sortAscending, internalColumns], (newValues, prevValues) => {
       newValues;
       prevValues;
 
-      console.log(sortCriterion.value, sortAscending.value);
+      internalRows.value = props.rows.filter((r : any) => {
+          const mismatchedFilters = internalColumns.value
+                .filter(c => c.filter != "")
+                .filter(c => {
+                    if (r[`${c.prop}`].includes(c.filter)) return false;
+                    else return true;
+                });
+          return mismatchedFilters.length == 0 ? true : false;
+      })
 
-      internalRows.value = internalRows.value.sort((a, b) => {
-        const firstProp = a[sortCriterion.value];
-        const secondProp = b[sortCriterion.value];
+      if(sortCriterion.value != "") {
+        internalRows.value = internalRows.value.sort((a, b) => {
+            const firstProp = a[sortCriterion.value];
+            const secondProp = b[sortCriterion.value];
 
-        return firstProp.localeCompare(secondProp) * (sortAscending.value ? 1 : -1);
-      });
-    });
+            return firstProp.localeCompare(secondProp) * (sortAscending.value ? 1 : -1);
+        });
+      }
+    },
+    { deep: true });
 
-    return { sortCriterion, sortAscending, internalRows };
+    return { sortCriterion, sortAscending, internalRows, internalColumns };
   },
 };
 </script>
